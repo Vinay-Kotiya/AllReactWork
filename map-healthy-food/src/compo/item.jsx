cc4f265a253270525c09be47966722d06105c4ea
vinay_k <vinaykotiya77@gmail.com>
2024-08-23 12:08:26 +0530

    23aug work

--- map-healthy-food/src/compo/item.jsx
+++ map-healthy-food/src/compo/item.jsx
@@ -2,14 +2,16 @@ import styles from "./item.module.css";
 const Item=({fooditems})=>{
     const heandlerbuybutton=(event)=>{
         console.log(event);
-        console.log(`${fooditems}  Button has clicked`)
+        console.log(`${fooditems}  Button has clicked`);
+
     }
+   
     return <>
        <hr></hr>
       
         <li className={`list-group-item`}>
         <span>  {fooditems}
-        <button className={styles.button} onClick={(event)=>heandlerbuybutton(event)}>Buy</button>
+        <button className={styles.button} onKeyDown={(event)=>heandlerbuybutton(event)}>Buy</button>
         </span>
         </li>
         {/* className={`${style["list-group"]}`} */}
